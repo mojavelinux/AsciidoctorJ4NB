429c55967cfd14ebda931607e823b00e69075d08
Jesse Glick <jglick@cloudbees.com>
2014-12-09 11:11:54 -0500

    Converted to Maven (fixes #3).

--- AsciidoctorJ4NB/src/org/netbeans/asciidoc/package-info.java
+++
@@ -1,8 +0,0 @@
-@TemplateRegistrations({
-    @TemplateRegistration(folder = "AsciiDoc", displayName = "Simple", content = "resources/AsciiDoc.adoc", requireProject = false),
-    @TemplateRegistration(folder = "AsciiDoc", displayName = "Admonition", content = "resources/Admonition.adoc", requireProject = false),
-})
-package org.netbeans.asciidoc;
-
-import org.netbeans.api.templates.TemplateRegistration;
-import org.netbeans.api.templates.TemplateRegistrations;
---
+++ src/main/java/org/netbeans/asciidoc/package-info.java
@@ -0,0 +1,8 @@
+@TemplateRegistrations({
+    @TemplateRegistration(folder = "AsciiDoc", displayName = "Simple", content = "resources/AsciiDoc.adoc", requireProject = false),
+    @TemplateRegistration(folder = "AsciiDoc", displayName = "Admonition", content = "resources/Admonition.adoc", requireProject = false),
+})
+package org.netbeans.asciidoc;
+
+import org.netbeans.api.templates.TemplateRegistration;
+import org.netbeans.api.templates.TemplateRegistrations;
